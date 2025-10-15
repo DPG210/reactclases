86237c82d845cfd15699ba8e0d62fd18daa5b1c9
DPG210 <diegogrego210@gmail.com>
2025-10-15 09:22:01 +0200

    Padre e hijo deportes completo

--- src/components/HijoDeporte.js
+++ src/components/HijoDeporte.js
@@ -2,21 +2,21 @@ import { Component } from "react";
 
 class HijoDeporte extends Component {
 
-    state={
-        favorito:""
+    
+    seleccionarFavoritos=()=>{
+        //CAPTURAMOS EL DEPORTE DEL PROPIO COMPONENT EN PROPS
+        var deporte=this.props.nombre;
+        //REALIZAMOS LA PETICION AL PADRE
+        console.log(deporte);
+        this.props.mostrarFavorito(deporte);
     }
-
-    mostrarFavorito=()=>{
-        this.setState({
-            favorito:this.props.nombre
-        })
-    }
+    
 
     render(){
         return(<div>
             <h3 style={{color:"blue"}}>Deporte: {this.props.nombre}</h3>
-            <h2 style={{backgroundColor:"yellowgreen"}}>Su deporte favorito es: {this.state.favorito}</h2>
-            <button onClick={this.mostrarFavorito}>Mostrar favorito</button>
+            
+            <button onClick={this.seleccionarFavoritos}>Mostrar favorito</button>
         </div>)
     }
 }
--- src/components/PadreDeportes.js
+++ src/components/PadreDeportes.js
@@ -5,13 +5,25 @@ import HijoDeporte from "./HijoDeporte";
 class PadreDeportes extends Component {
 
     deportes=["Curling","Petanca","Poker","Futbol"];
-
+    //NECESITAMOS UNA VARIABLE STAGE PARA VISUALIZAR EL DEPORTE FAVORITO
+    state={
+        favorito:""
+    }
+    //DEBEMOS TENER UN METODO QUE SERA INVOCADO POR EL HIJO AL PULSAR 
+    // SOBRE EL BOTON DE SELECCIONAR
+    mostrarFavorito=(deporteSeleccionado)=>{
+        this.setState({
+            favorito:deporteSeleccionado
+        })
+    }
     render(){
         return(<div>
             <h1>Padre deportes</h1>
+            <h4 style={{backgroundColor:"green"}}>Su favorito es {this.state.favorito}</h4>
             {
                 this.deportes.map((deporte,index)=>{
-                    return(<HijoDeporte key={index} nombre={deporte}/>)
+                    return(<HijoDeporte key={index} nombre={deporte}
+                    mostrarFavorito={this.mostrarFavorito}/>)
                 })
             }
         </div>)
